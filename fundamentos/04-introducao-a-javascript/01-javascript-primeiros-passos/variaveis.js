c7db8d768d07d3852b7db3946cd2cc61b0779e19
Veiga <gabrielvalmeidaa@gmail.com>
2022-10-19 18:29:15 -0300

    Dia 01 Javascript - Variáveis

---
+++ fundamentos/04-introducao-a-javascript/01-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,16 @@
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+// Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+// Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+
+const myName = 'Carolina';
+const birthCity = 'Itaboraí';
+let birthYear = 1999;
+console.log(myName, birthCity, birthYear)
+birthYear = 2030;
+console.log(birthYear)
+birthCity = 'RJ';
+console.log(birthCity) 
+// Deu erro porque birthCity é uma constante, e constante não pode ser alterada.
